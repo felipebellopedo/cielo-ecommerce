5eb946cda6a7f49a962321b1dc191866715d3c65
felipebellopedo <felipebellopedo@gmail.com>
2017-06-08 12:19:27 -0300

    Add files via upload

---
+++ arqint.php
@@ -0,0 +1,11 @@
+public Transacao criarTransacao(Pedido pedido) throws FalhaComunicaoException {
+Mensagem mensagem = new MensagemNovaTransacao (loja, pedido);
+Requisicao requisicao = new Requisicao(mensagem);
+return requisicao.enviarPara(destino);
+}
+public Transacao capturar(Transacao transacao, long valor) throws FalhaComunicaoException
+{
+Mensagem mensagem = new MensagemCaptura(loja, transacao, valor);
+Requisicao requisicao = new Requisicao(mensagem);
+return requisicao.enviarPara(destino);
+}
---
+++ arqtrans.php
@@ -0,0 +1,7 @@
+POST /servicos/ecommwsec.do HTTP/1.1
+Host: ecommerce.cielo.com.br
+Content-Type: application/x-www-form-urlencoded
+Content-Length: length
+mensagem=<?xml version="1.0" encoding="ISO-8859-1"?><requisicao-captura id="3e22bdd0-
+2017-4756-80b7-35a532e6c973" versao="1.2.1"><tid>10069930690101012005</tid><dadosec><numero>1006993069</numero><chave>25fbb99741c739dd84d7b06ec78c9bac718838630f30b112d033
+ce2e621b34f3</chave></dados-ec><valor>3880</valor></requisicao-captura>
